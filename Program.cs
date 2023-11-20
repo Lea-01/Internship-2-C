f02df99f700eb0ef8521b18414dc0b7090b3c8f7
Lea-01 <149975598+Lea-01@users.noreply.github.com>
2023-11-20 22:10:13 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿int firstInt = 2,
+    secondInt = 3;
+double dobuleNum = 1.22;
+float floatNum = 1.22f
+decimal decimalNum = 2.2m;
+
+bool trueOrFalse = true;
+
+char someChar = "a";
+string someString = "Hello World";
+
+DateTime date = new DateTime(2022, 01, 01, 2, 2, 2);
+
+Console.WriteLine("Hello World");
+var someString = "Hello";
+Console.WriteLine(someString);
+
+var firstInt = 5;
+var secondInt = 3;
+
+Console.WriteLine(firstInt + secondInt);
+Console.WriteLine(firstInt - secondInt);
+Console.WriteLine(firstInt * secondInt);
+Console.Clear  ();
+firstInt++;
+++firstInt;
+Console.WriteLine(firstInt++);
+Console.WriteLine(firstInt);
+
+var result2 = (double)firstInt 
+
+Console.WriteLine();
+var newInt = int.Parse(console.);
+var newDouble = double.Parse(Console.ReadLine());
+
+    
